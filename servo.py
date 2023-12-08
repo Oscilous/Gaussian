2d6605256a5a3de91d2bc94d34be6a12b2edc02d
oscilous <arfil21@student.sdu.dk>
2023-12-08 13:14:39 +0000

    Tried servo, but unreliable

--- servo.py
+++ servo.py
@@ -6,17 +6,26 @@ GPIO.setwarnings(False)
 servo_pin = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin,GPIO.OUT)
-pwm = GPIO.PWM(servo_pin,50) 
+pwm = GPIO.PWM(servo_pin,50)
+
 pwm.start(0) #init af servo motor
 time.sleep(1)
 try:
     while True:
-        pwm.ChangeDutyCycle(1)
-        time.sleep(3)
-        pwm.ChangeDutyCycle(1.5)
-        time.sleep(3)
-        pwm.ChangeDutyCycle(2)
-        time.sleep(3)
+        pwm.ChangeDutyCycle(2.5)
+        #pwm.stop()
+        time.sleep(2)
+        #pwm.start(0)
+        pwm.ChangeDutyCycle(7.5)
+        #pwm.stop()
+        time.sleep(2)
+        #pwm.start(0)
+        pwm.ChangeDutyCycle(12.5)
+        #pwm.stop()
+        time.sleep(2)
+        #pwm.start(0)
+        
+        
 
 except KeyboardInterrupt:
     print("Exiting gracefully")
