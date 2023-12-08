1e5fae36a168f9053094de271ac6168a356bd240
oscilous <arfil21@student.sdu.dk>
2023-12-08 15:58:51 +0000

    Working stepper based homing, never cleanup()

--- stepper.py
+++ stepper.py
@@ -2,36 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 # Set GPIO pin numbers
-DIR_PIN = 2  # Replace with your chosen GPIO pin number for direction
-STEP_PIN = 3 # Replace with your chosen GPIO pin number for step
-
+DIR_PIN = 23  # Replace with your chosen GPIO pin number for direction
+STEP_PIN = 24 # Replace with your chosen GPIO pin number for step
+end_switch = 25
+EN_PIN = 7
+GPIO.cleanup()
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR_PIN, GPIO.OUT)
 GPIO.setup(STEP_PIN, GPIO.OUT)
-
+GPIO.setup(EN_PIN, GPIO.OUT)
+GPIO.setup(end_switch, GPIO.IN, GPIO.PUD_UP)
 try:
-    while True:
-        GPIO.output(DIR_PIN, GPIO.HIGH)  # Set to GPIO.LOW for opposite direction
-
-        # Step the motor
-        for _ in range(100):
-            GPIO.output(STEP_PIN, GPIO.HIGH)
-            time.sleep(0.001)
-            GPIO.output(STEP_PIN, GPIO.LOW)
-            time.sleep(0.001)
-        GPIO.output(DIR_PIN, GPIO.LOW)  # Set to GPIO.LOW for opposite direction
-
-        # Step the motor
-        for _ in range(100):
-            GPIO.output(STEP_PIN, GPIO.HIGH)
-            time.sleep(0.001)
-            GPIO.output(STEP_PIN, GPIO.LOW)
-            time.sleep(0.001)
-
+    GPIO.output(EN_PIN, GPIO.LOW)
+    GPIO.output(DIR_PIN, GPIO.LOW)  # Set to GPIO.LOW for opposite direction
+    while GPIO.input(end_switch) == GPIO.LOW:     
+        GPIO.output(STEP_PIN, GPIO.HIGH)
+        time.sleep(0.001)
+        GPIO.output(STEP_PIN, GPIO.LOW)
+        time.sleep(0.001)
+    # Cleanup GPIO
+    GPIO.output(STEP_PIN, GPIO.LOW)
+    GPIO.output(EN_PIN, GPIO.HIGH)
+        
+        
 except KeyboardInterrupt:
     print("Exiting program")
 
 finally:
+    pass
+    #DO NOT CLEAN UP, if cleaned stepper will float
     # Cleanup GPIO
-    GPIO.cleanup()
+    #GPIO.cleanup()
