272dc63bf03e6b987cf8c7e7395a37d39d4541cf
oscilous <arfil21@student.sdu.dk>
2023-12-12 08:38:39 +0000

    Moved on from servo

--- button.py
+++ button.py
@@ -4,13 +4,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 button = 25
-
+solunoid = 22
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(solunoid, GPIO.OUT)
+try:
+    while True:
+        button_state = GPIO.input(button)
+        #if button_state == GPIO.HIGH:
+        GPIO.output(solunoid, GPIO.HIGH)
+        time.sleep(5)
+        #else:
+        GPIO.output(solunoid, GPIO.LOW)
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Exiting program")
 
-while True:
-    button_state = GPIO.input(button)
-    if button_state == GPIO.HIGH:
-      print ("HIGH")
-    else:
-      print ("LOW")
-    time.sleep(0.5)
+finally:
+    pass
+    #GPIO.cleanup()
--- stepper.py
+++ stepper.py
@@ -1,29 +1,67 @@
 import RPi.GPIO as GPIO
 import time
 
+speed = 0.005
 # Set GPIO pin numbers
+solunoid = 22
 DIR_PIN = 23  # Replace with your chosen GPIO pin number for direction
 STEP_PIN = 24 # Replace with your chosen GPIO pin number for step
 end_switch = 25
-EN_PIN = 7
 GPIO.cleanup()
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR_PIN, GPIO.OUT)
 GPIO.setup(STEP_PIN, GPIO.OUT)
-GPIO.setup(EN_PIN, GPIO.OUT)
+GPIO.setup(solunoid, GPIO.OUT)
+GPIO.output(solunoid, GPIO.LOW)
 GPIO.setup(end_switch, GPIO.IN, GPIO.PUD_UP)
-try:
-    GPIO.output(EN_PIN, GPIO.LOW)
-    GPIO.output(DIR_PIN, GPIO.LOW)  # Set to GPIO.LOW for opposite direction
+
+def home():
+    GPIO.output(DIR_PIN, GPIO.LOW)  # To end stop
     while GPIO.input(end_switch) == GPIO.LOW:     
         GPIO.output(STEP_PIN, GPIO.HIGH)
-        time.sleep(0.001)
+        time.sleep(0.02)
         GPIO.output(STEP_PIN, GPIO.LOW)
-        time.sleep(0.001)
-    # Cleanup GPIO
-    GPIO.output(STEP_PIN, GPIO.LOW)
-    GPIO.output(EN_PIN, GPIO.HIGH)
+        time.sleep(0.02)
+    GPIO.output(DIR_PIN, GPIO.HIGH)  # Away from endstop
+    for i in range (0, 8):     
+        GPIO.output(STEP_PIN, GPIO.HIGH)
+        time.sleep(speed)
+        GPIO.output(STEP_PIN, GPIO.LOW)
+        time.sleep(speed)
+        
+def dispense_pellet():
+    GPIO.output(DIR_PIN, GPIO.HIGH)  # Away from endstop
+    for i in range (0, 50):
+        GPIO.output(STEP_PIN, GPIO.HIGH)
+        time.sleep(speed)
+        GPIO.output(STEP_PIN, GPIO.LOW)
+        time.sleep(speed)
+def return_entrance():
+    GPIO.output(DIR_PIN, GPIO.LOW)  # To  endstop
+    for i in range (0, 100):
+        GPIO.output(STEP_PIN, GPIO.HIGH)
+        time.sleep(speed)
+        GPIO.output(STEP_PIN, GPIO.LOW)
+        time.sleep(speed)
+        
+try:
+    home()
+    i = 0
+    while True:
+        
+        time.sleep(1)
+        dispense_pellet()
+        time.sleep(1)
+        if i == 1:
+            GPIO.output(solunoid, GPIO.HIGH)
+        dispense_pellet()
+        time.sleep(1)
+        if i == 1:
+            GPIO.output(solunoid, GPIO.LOW)
+            i = 0
+        return_entrance()
+        i = i + 1
         
         
 except KeyboardInterrupt:
