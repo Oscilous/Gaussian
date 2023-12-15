0c9a2be67d6a35461c01e32a7d79c0c6b45f4fe5
Oscilous <arfil21@student.sdu.dk>
2023-12-15 15:50:19 +0100

    Manual focus

--- cam3_duo.py
+++ cam3_duo.py
@@ -1,33 +1,18 @@
 import time
-
-import numpy as np
-
 from picamera2 import Picamera2, Preview
 
 picam2a = Picamera2(0)
 picam2a.configure(picam2a.create_preview_configuration())
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
 picam2a.start_preview(Preview.QTGL)
 
 picam2b = Picamera2(1)
 picam2b.configure(picam2b.create_preview_configuration())
+picam2b.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
 picam2b.start_preview(Preview.QT)
 
 picam2a.start()
 picam2b.start()
-time.sleep(10)
+time.sleep(100)
 picam2a.stop_preview()
 picam2b.stop_preview()
-
-time.sleep(2)
-
-overlay = np.zeros((300, 400, 4), dtype=np.uint8)
-overlay[:150, 200:] = (255, 0, 0, 64)
-overlay[150:, :200] = (0, 255, 0, 64)
-overlay[150:, 200:] = (0, 0, 255, 64)
-
-picam2a.start_preview(Preview.QTGL)
-picam2b.start_preview(Preview.QT)
-picam2a.set_overlay(overlay)
-picam2b.set_overlay(overlay)
-
-time.sleep(10)
