bbad4d617cae40ab34ce94505b5b87d974b6523f
Oscilous <arfil21@student.sdu.dk>
2023-12-15 16:52:35 +0100

    Picamera2

--- cam3_duo.py
+++ cam3_duo.py
@@ -2,14 +2,33 @@ import time
 from picamera2 import Picamera2, Preview
 from libcamera import controls
 
+IMG_DIMS = (960, 960)
+
 first_camera = Picamera2(0)
-first_camera.configure(first_camera.create_preview_configuration())
-first_camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
+first_camera.configure(first_camera.create_preview_configuration(main={"size": (960, 960)}))
+
+config = first_camera.create_preview_configuration()
+config['main']['size'] = IMG_DIMS
+config['main']['format'] = "YUV420"
+first_camera.align_configuration(config)
+print(config)
+first_camera.configure(config)
+
+#first_camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
+#camera.shutter_speed = 10000
+first_camera.set_controls({"ExposureTime": 10000, "AnalogueGain": 1.0})
+#first_camera.meter_mode = 'backlight'  # picamera uses 'meter_mode' instead of 'exposure_mode'
+first_camera.set_controls({"AeConstraintMode": "Shadows"})
+#first_camera.exposure_mode = 'off'
+first_camera.set_controls({"AeEnable": "False"})
+#first_camera.awb_mode = 'fluorescent'
+first_camera.set_controls({"AwbMode": "Fluorescent"})
+
 first_camera.start_preview(Preview.QTGL)
+
 
 second_camera = Picamera2(1)
 second_camera.configure(second_camera.create_preview_configuration())
-second_camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
 second_camera.start_preview(Preview.QT)
 
 first_camera.start()
