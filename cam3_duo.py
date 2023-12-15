21e50d818b2f76c66acc30901c13a1b3feab29ba
Oscilous <arfil21@student.sdu.dk>
2023-12-15 15:52:35 +0100

    Naming

--- cam3_duo.py
+++ cam3_duo.py
@@ -1,18 +1,18 @@
 import time
 from picamera2 import Picamera2, Preview
 
-picam2a = Picamera2(0)
-picam2a.configure(picam2a.create_preview_configuration())
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
-picam2a.start_preview(Preview.QTGL)
+first_camera = Picamera2(0)
+first_camera.configure(picam2a.create_preview_configuration())
+first_camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
+first_camera.start_preview(Preview.QTGL)
 
-picam2b = Picamera2(1)
-picam2b.configure(picam2b.create_preview_configuration())
-picam2b.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
-picam2b.start_preview(Preview.QT)
+second_camera = Picamera2(1)
+second_camera.configure(picam2b.create_preview_configuration())
+second_camera.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 10.0})
+second_camera.start_preview(Preview.QT)
 
-picam2a.start()
-picam2b.start()
+first_camera.start()
+second_camera.start()
 time.sleep(100)
-picam2a.stop_preview()
-picam2b.stop_preview()
+first_camera.stop_preview()
+second_camera.stop_preview()
