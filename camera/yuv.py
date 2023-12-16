2b506b712842e1c39f2a9870a182906d97357b71
Oscilous <arfil21@student.sdu.dk>
2023-12-16 11:03:11 +0100

    Yuv fixing

--- camera/yuv.py
+++ camera/yuv.py
@@ -15,13 +15,14 @@ try:
     while True:
         im = picam2.capture_array()
         img_preproc = im[:IMG_DIMS[1], :IMG_DIMS[0]]
+        img_preproc = cv2.resize(img_preproc, (IMG_DIMS[0], IMG_DIMS[1]))
         mask = np.zeros(IMG_DIMS, dtype="uint8")
         # Draw a circle based on the trackbar values
         cv2.circle(mask, (1500, 1000), 1000, 255, -1)
         masked_image = cv2.bitwise_and(img_preproc, mask)
 
         # Apply thresholding to create a binary image
-        _, binary = cv2.threshold(masked_image, 240, 255, cv2.THRESH_BINARY)
+        _, binary = cv2.threshold(masked_image, 180, 255, cv2.THRESH_BINARY)
         cv2.imshow("Camera", binary)
 
         # Save an image when a key is pressed (e.g., 's')
