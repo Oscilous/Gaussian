2d042c6b4e43bb7315d36b4b6ace2fda54489aaf
Oscilous <arfil21@student.sdu.dk>
2023-12-16 15:22:48 +0100

    Sorting and processing

--- camera_adjusting/threshold.py
+++ camera_adjusting/threshold.py
@@ -145,7 +145,7 @@ def count_black_pixels(binary_image, mask):
     else:
         print("GOOD")
 
-picture = "shadows"
+picture = "aeenable"
 # Create the Trackbars, so the mask can be created
 create_trackbars()
 # Main loop
