4dcd5d121b545434b6472f9d36c2e11ad4fc4b69
Oscilous <arfil21@student.sdu.dk>
2023-12-16 10:54:34 +0100

    Masked test

--- camera/cvgreyscale.py
+++ camera/cvgreyscale.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from picamera2 import Picamera2
 
 IMG_DIMS = (3280, 2464)
@@ -14,7 +15,14 @@ try:
     while True:
         im = picam2.capture_array()
         img_preproc = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("Camera", img_preproc)
+        mask = np.zeros(IMG_DIMS, dtype="uint8")
+        # Draw a circle based on the trackbar values
+        cv2.circle(mask, (1500, 1000), 1000, 255, -1)
+        masked_image = cv2.bitwise_and(img_preproc, mask)
+
+        # Apply thresholding to create a binary image
+        _, binary = cv2.threshold(masked_image, 240, 255, cv2.THRESH_BINARY)
+        cv2.imshow("Camera", binary)
 
         # Save an image when a key is pressed (e.g., 's')
         key = cv2.waitKey(1)
