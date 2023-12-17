c733b0145a21ec11dd45d96089f6550c3414da61
Oscilous <arfil21@student.sdu.dk>
2023-12-17 22:25:18 +0100

    Exposure

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ picam2.preview_configuration.main.size = IMG_DIMS
 picam2.preview_configuration.main.format = "YUV420"
 picam2.preview_configuration.align()
 picam2.configure("preview")
-picam2.set_controls({"ExposureTime": 12000})
+picam2.set_controls({"ExposureTime": 1000})
 picam2.start()
 
 second_camera = Picamera2(0)
@@ -252,7 +252,7 @@ second_camera.preview_configuration.main.size = IMG_DIMS
 second_camera.preview_configuration.main.format = "YUV420"
 second_camera.preview_configuration.align()
 second_camera.configure("preview")
-second_camera.set_controls({"ExposureTime": 12000})
+second_camera.set_controls({"ExposureTime": 1000})
 second_camera.start()
 
 # Create the Trackbars, so the mask can be created
