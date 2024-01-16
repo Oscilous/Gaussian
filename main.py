a55b1df182b2184abf15b6c3f0748eeffe59278b
Oscilous <arfil21@student.sdu.dk>
2024-01-16 13:12:19 +0100

    Update pellet detection threshold in is_pellet_present function

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ def is_pellet_present(image, mask):
     update_window()
     # Apply thresholding to create a binary image
     #_, binary = cv2.threshold(masked_image, 100, 220, cv2.THRESH_BINARY)
-    binary = cv2.inRange(masked_image, 100, 230)
+    binary = cv2.inRange(masked_image, 100, 220)
     impurity_pixel_count = np.sum(binary == 255)
     area_pixel_count = np.sum(mask == 255)
     detection_threshold = cv2.getTrackbarPos("detection_threshold", "Trackbars")
